ca0dd3f1d94150af40df0d7a9b36b3147cf76d2d
niksiboxi <niksiboxi@hotmail.com>
2025-01-28 10:59:15 +0200

    Include bluez library

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <time.h>
 
-int main (void)
+int device;
+
+int main(void)
 {
-    printf("Hello World!\n");
+    device = hci_open_dev(1);
+
+    if (device < 0)
+    {
+        device = hci_open_dev(0);
+        if (device >= 0)
+        {
+            printf("Using hci0\n");
+        }
+    }
+    else
+    {
+        printf("Using hci1\n");
+    }
 
     return 0;
 }
